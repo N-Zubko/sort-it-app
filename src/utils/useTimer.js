950cef2dde4d892f8c6073c03a338e62ac24b056
Nadia Zubko <ne.nadezda@gmail.com>
2022-10-20 13:51:35 -0600

    lift seconds to parent element, reset time when Modal is closed

--- src/utils/useTimer.js
+++ src/utils/useTimer.js
@@ -1,4 +1,4 @@
-import { useCallback, useState, useContext } from 'react';
+import { useCallback, useState, useContext, useEffect } from 'react';
 import { interval } from './interval';
 import { Context } from '../components/Context';
 import { wasteToSort } from '../components/ItemTypes';
@@ -6,16 +6,20 @@ import { wasteToSort } from '../components/ItemTypes';
 const use1Second = interval(1e3);
 
 export const useTimer = ({
-  seconds: initialSeconds = 0,
+  // seconds: initialSeconds = 0,
   running: initiallyRunning = false,
 } = {}) => {
-  const [seconds, setSeconds] = useState(initialSeconds);
+  // const [seconds, setSeconds] = useState(initialSeconds);
   const [running, setRunning] = useState(initiallyRunning);
+  const { seconds, setSeconds } = useContext(Context);
   const { startSorting, setStartSorting } = useContext(Context);
+  const { showModal, setShowModal } = useContext(Context);
+
   const tick = useCallback(
     () => (running ? setSeconds((seconds) => seconds + 1) : undefined),
     [running]
   );
+
   const start = () => {
     setRunning(true);
     setStartSorting(true);
@@ -31,6 +35,18 @@ export const useTimer = ({
   };
 
   use1Second(tick);
+
+  useEffect(() => {
+    if (!startSorting) {
+      pause();
+    }
+  }, [startSorting]);
+
+  useEffect(() => {
+    if (!showModal) {
+      reset();
+    }
+  }, [showModal]);
 
   return { pause, reset, running, seconds, start, stop };
 };
