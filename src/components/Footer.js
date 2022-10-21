3b8dc14f42c36bf3338bfbe4ac6e6ad1a41b00da
Nadia Zubko <ne.nadezda@gmail.com>
2022-10-21 14:26:13 -0600

    create Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,30 @@
+// TODO: Add github and linkedin icons, links to my profiles
+import { ReactComponent as LinkedInIcon } from '../pictures/linkedin.svg';
+import { ReactComponent as GitHubIcon } from '../pictures/github.svg';
+import { LibraryIcon } from '@heroicons/react/outline';
+
+const Footer = () => {
+  return (
+    <div className="flex flex-row flex-wrap justify-center">
+      <footer className="text-center">
+        Sort_it! app developed with ❤️ by Nadia Zubko © 2022
+      </footer>
+      <a
+        href="https://github.com/N-Zubko/sort-it-app"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <GitHubIcon />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/nadezhda-zubko-developer/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <LinkedInIcon />
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
