91817d30da98cef2c538f3c24d1b4b49baa82a6f
Nadia Zubko <ne.nadezda@gmail.com>
2022-10-21 21:58:27 -0600

    add Card to Learning to display waste info

--- src/pages/Learn.js
+++ src/pages/Learn.js
@@ -1,5 +1,23 @@
+import { useState } from 'react';
+import Input from '../components/Input';
+import Spinner from '../components/Spinner';
+import { Card } from '../components/Card';
+import { wasteToSort } from '../components/ItemTypes';
+
 const Learn = () => {
-  return <>Learn Page</>;
+  return (
+    <div className="grid grid-rows-4 grid-flow-col gap-4 ml-5 mr-5">
+      {wasteToSort.map((item) => (
+        <Card
+          id={item.id}
+          name={item.name}
+          description={item.description}
+          img={item.url}
+          wasteType={item.wasteType}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default Learn;
