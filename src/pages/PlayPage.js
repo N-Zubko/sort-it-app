05e4fcff1237185133c4928d34fcaa2a7918ae39
Nadia Zubko <ne.nadezda@gmail.com>
2022-10-25 14:01:14 -0600

    remove redundant code

--- src/pages/PlayPage.js
+++ src/pages/PlayPage.js
@@ -1,15 +1,15 @@
-import styles from '../styles/pages/Dashboard.module.css';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
-import { useOutletContext } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 import Board from '../components/Board';
 
-const PlayPage = () => {
-  const { user } = useOutletContext();
+import { WasteQuery } from '../graphQL/WasteQuery';
 
+const PlayPage = () => {
   return (
     <div className="flex flex-col flex-wrap items-center justify-center">
+      <WasteQuery />
+
       <DndProvider backend={HTML5Backend}>
         <Helmet>
           <title>Sort_it!</title>
