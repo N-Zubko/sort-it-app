e2c1d8f8cf8b87bb8e9e075b99b38397af1cc437
Nadia Zubko <ne.nadezda@gmail.com>
2022-10-25 14:02:12 -0600

    add NhostApolloProvider to App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
+import { NhostApolloProvider } from '@nhost/react-apollo';
 
 import Layout from './components/Layout';
 import PlayPage from './pages/PlayPage';
@@ -12,20 +13,22 @@ const nhost = new NhostClient({
   subdomain: process.env.REACT_APP_NHOST_SUBDOMAIN,
   region: process.env.REACT_APP_NHOST_REGION,
 });
-
+// console.log('nhost', nhost);
 function App() {
   return (
     <NhostReactProvider nhost={nhost}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<PlayPage />} />
-            <Route path="about" element={<About />} />
-            <Route path="learn" element={<Learn />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-      <Toaster />
+      <NhostApolloProvider nhost={nhost}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<PlayPage />} />
+              <Route path="about" element={<About />} />
+              <Route path="learn" element={<Learn />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+        <Toaster />
+      </NhostApolloProvider>
     </NhostReactProvider>
   );
 }
