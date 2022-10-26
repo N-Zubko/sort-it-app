aec5a9a97f1650cd79aaa463fedbde8074a6db9e
Nadia Zubko <ne.nadezda@gmail.com>
2022-10-26 11:38:23 -0600

    fetch items for Learn page using GraphQL

--- src/pages/Learn.js
+++ src/pages/Learn.js
@@ -1,27 +1,57 @@
-// import Spinner from '../components/Spinner';
 import { Card } from '../components/Card';
-import { wasteToSort } from '../components/ItemTypes';
+import { useQuery } from '@apollo/client';
+import { GET_WASTE_DETAILED } from '../graphql/WasteQuery';
+import Spinner from '../components/Spinner';
 
 const Learn = () => {
+  const { loading, error, data } = useQuery(GET_WASTE_DETAILED);
+
+  if (loading) return <Spinner />;
+
+  if (error) return `Error! ${error}`;
+
   return (
     <>
-      <h2 className="text-center mb-2 mt-6 text-lg font-semibold">
-        Learn How to Sort Waste
-      </h2>
-      <div className="grid grid-cols-1 md:gap-6 md:grid-cols-2 lg:grid-cols-4 mb-64">
-        {wasteToSort.map((item) => (
-          <Card
-            key={item.id}
-            id={item.id}
-            name={item.name}
-            description={item.description}
-            url={item.url}
-            wasteType={item.wasteType}
-          />
-        ))}
-      </div>
+      {data.wasteToSorts.edges.map((item) => (
+        <Card
+          key={item.node.id}
+          id={item.node.id}
+          name={item.node.name}
+          description={item.node.description}
+          url={item.node.image.url}
+          wasteType={item.node.wasteType}
+        />
+      ))}
     </>
   );
+
+  //   if (data) {
+  //     setLoad(data.wasteToSorts.edges);
+  //   }
+  // }, [data]);
+
+  // return (
+  //   <>
+  //     <h2 className="text-center mb-2 mt-6 text-lg font-semibold">
+  //       Learn How to Sort Waste
+  //     </h2>
+  //     <div className="grid grid-cols-1 md:gap-6 md:grid-cols-2 lg:grid-cols-4 mb-64">
+  //       {load.length > 1 ? (
+  //         load.map((item) => (
+  //           <Card
+  //             key={item.node.id}
+  //             id={item.node.id}
+  //             name={item.node.name}
+  //             description={item.node.description}
+  //             url={item.node.image.url}
+  //             wasteType={item.node.wasteType}
+  //           />
+  //         ))
+  //       ) : (
+  //         <Spinner />
+  //       )}
+  //     </div>
+  //   </>
 };
 
 export default Learn;
