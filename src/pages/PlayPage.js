fd7ea13c47cc6ba928f59d9bb84bc84f47102166
Nadia Zubko <ne.nadezda@gmail.com>
2022-10-26 11:39:05 -0600

    remove redundant code

--- src/pages/PlayPage.js
+++ src/pages/PlayPage.js
@@ -3,13 +3,9 @@ import { HTML5Backend } from 'react-dnd-html5-backend';
 import { Helmet } from 'react-helmet';
 import Board from '../components/Board';
 
-import { WasteQuery } from '../graphQL/WasteQuery';
-
 const PlayPage = () => {
   return (
     <div className="flex flex-col flex-wrap items-center justify-center">
-      {/* <WasteQuery /> */}
-
       <DndProvider backend={HTML5Backend}>
         <Helmet>
           <title>Sort_it!</title>
