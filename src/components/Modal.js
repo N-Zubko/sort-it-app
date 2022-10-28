262789924a4b6eb805acc6695b787e50e5e17f55
Nadia Zubko <ne.nadezda@gmail.com>
2022-10-28 12:09:35 -0600

    remove redundant code

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,19 +2,11 @@ import { useContext } from 'react';
 import { Context } from './Context';
 
 export default function Modal() {
-  //   const [showModal, setShowModal] = useState(false);
   const { showModal, setShowModal } = useContext(Context);
   const { seconds } = useContext(Context);
 
   return (
     <>
-      {/* <button
-        className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-        type="button"
-        onClick={() => setShowModal(true)}
-      >
-        Open modal
-      </button> */}
       {showModal ? (
         <>
           <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
