d661bebd031d9cae20662fc80420e65c57cbc575
Nadia Zubko <ne.nadezda@gmail.com>
2022-10-28 11:50:06 -0600

    Change styling and add heading

--- src/pages/Learn.js
+++ src/pages/Learn.js
@@ -12,46 +12,23 @@ const Learn = () => {
 
   return (
     <>
-      {data.wasteToSorts.edges.map((item) => (
-        <Card
-          key={item.node.id}
-          id={item.node.id}
-          name={item.node.name}
-          description={item.node.description}
-          url={item.node.image.url}
-          wasteType={item.node.wasteType}
-        />
-      ))}
+      <h2 className="text-center mb-2 mt-6 text-lg font-semibold">
+        Learn How to Sort Waste
+      </h2>
+      <div className="grid grid-cols-1 sm:gap-2 md:gap-6 md:grid-cols-2 lg:grid-cols-4 mb-64">
+        {data.wasteToSorts.edges.map((item) => (
+          <Card
+            key={item.node.id}
+            id={item.node.id}
+            name={item.node.name}
+            description={item.node.description}
+            url={item.node.image.url}
+            wasteType={item.node.wasteType}
+          />
+        ))}
+      </div>
     </>
   );
-
-  //   if (data) {
-  //     setLoad(data.wasteToSorts.edges);
-  //   }
-  // }, [data]);
-
-  // return (
-  //   <>
-  //     <h2 className="text-center mb-2 mt-6 text-lg font-semibold">
-  //       Learn How to Sort Waste
-  //     </h2>
-  //     <div className="grid grid-cols-1 md:gap-6 md:grid-cols-2 lg:grid-cols-4 mb-64">
-  //       {load.length > 1 ? (
-  //         load.map((item) => (
-  //           <Card
-  //             key={item.node.id}
-  //             id={item.node.id}
-  //             name={item.node.name}
-  //             description={item.node.description}
-  //             url={item.node.image.url}
-  //             wasteType={item.node.wasteType}
-  //           />
-  //         ))
-  //       ) : (
-  //         <Spinner />
-  //       )}
-  //     </div>
-  //   </>
 };
 
 export default Learn;
