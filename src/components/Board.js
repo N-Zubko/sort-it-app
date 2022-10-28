0ec267f56a5e5714a19166312b9485056d2d18ab
Nadia Zubko <ne.nadezda@gmail.com>
2022-10-28 11:54:28 -0600

    add a counter logic, fix conditions in useEffect, remove commented code

--- src/components/Board.js
+++ src/components/Board.js
@@ -10,46 +10,48 @@ import Timer from './Timer';
 import Modal from './Modal';
 
 import { Context } from './Context.js';
-import { wasteToSort } from './ItemTypes';
+import { useQuery } from '@apollo/client';
+import { GET_WASTE } from '../graphql/WasteQuery';
 
+import Spinner from './Spinner';
 import BackgroundImage from '../pictures/background.jpg';
-import DropSound from '../sounds/falling_garbage.wav';
 import TadaSound from '../sounds/tada.wav';
 import WasteSound from '../sounds/waste.wav';
 
-let newWasteArray = [...wasteToSort];
-
 function Board() {
   const [startSorting, setStartSorting] = useState(false);
+  const [showWasteDisplay, setShowWasteDisplay] = useState(false);
   const [wasteDisplay, setWasteDisplay] = useState([]);
   const [itemAdded, setItemAdded] = useState(false);
   const [showModal, setShowModal] = useState(false);
+  const [showSpinner, setShowSpinner] = useState(false);
   const [seconds, setSeconds] = useState(0);
+  const [count, setCount] = useState(0);
+
   const [playDrop] = useSound(WasteSound);
   const [playTada] = useSound(TadaSound);
+  const { loading, error, data } = useQuery(GET_WASTE);
 
-  const reduceWasteDisplay = (id) => {
-    const indexOfObject = newWasteArray.findIndex((object) => {
-      return object.id === id;
-    });
-    if (indexOfObject > -1) {
-      newWasteArray.splice(indexOfObject, 1);
-    }
-    setWasteDisplay([...newWasteArray]);
-    return;
-  };
   useEffect(() => {
-    if (newWasteArray.length > 0) {
-      setWasteDisplay([...newWasteArray]);
-      if (startSorting) playDrop();
-    } else if (newWasteArray.length === 0) {
+    if (startSorting && data) {
+      setWasteDisplay(data.wasteToSorts.edges);
+      setShowWasteDisplay(true);
+    }
+    if ((loading && startSorting) || error) setShowSpinner(true);
+  }, [startSorting, data, loading, error]);
+
+  useEffect(() => {
+    if (itemAdded && count < wasteDisplay.length - 1) {
+      setCount((count) => count + 1);
+      playDrop();
+    }
+    if (itemAdded && count === wasteDisplay.length - 1) {
+      setCount(count);
+      setStartSorting(false);
+      setShowWasteDisplay(false);
       playTada();
       setShowModal(true);
-      setStartSorting(false);
-      newWasteArray = [...wasteToSort];
-    } else {
-      newWasteArray = [...wasteToSort];
-      setStartSorting(false);
+      setCount(0);
     }
   }, [itemAdded]);
 
@@ -74,17 +76,18 @@ function Board() {
         style={{ backgroundImage: `url(${BackgroundImage})` }}
       >
         <Timer />
-        {wasteDisplay.length > 0 && startSorting ? (
+        {showSpinner && <Spinner />}
+        {wasteDisplay.length > 0 && showWasteDisplay ? (
           <div className="flex flex-col items-center justify-center self-center w-40 h-40 rounded-full bg-slate-50 hover:bg-emerald-100 mb-28">
             <WasteItem
-              key={wasteDisplay[0].id}
-              url={wasteDisplay[0].url}
-              id={wasteDisplay[0].id}
-              wasteType={wasteDisplay[0].wasteType}
-              name={wasteDisplay[0].name}
+              key={wasteDisplay[count].node.objectId}
+              url={wasteDisplay[count].node.image.url}
+              id={wasteDisplay[count].node.id}
+              wasteType={wasteDisplay[count].node.wasteType}
+              name={wasteDisplay[count].node.name}
             />
             <span className="text-center mt-2 bg-white pl-2 pr-2">
-              {wasteDisplay[0].name}
+              {wasteDisplay[count].node.name}
             </span>
           </div>
         ) : (
@@ -94,31 +97,19 @@ function Board() {
         )}
         <div className="flex flex-row flex-wrap justify-center content-end items-end">
           <div className="text-center mr-1 drop-shadow-lg">
-            <Landfill
-              wasteDisplay={wasteDisplay}
-              reduceWasteDisplay={reduceWasteDisplay}
-            />
+            <Landfill />
             <span className="bg-white pl-2 pr-2">Landfill Drop-off</span>
           </div>
           <div className="text-center drop-shadow-lg">
-            <BlueBin
-              wasteDisplay={wasteDisplay}
-              reduceWasteDisplay={reduceWasteDisplay}
-            />
+            <BlueBin />
             <span className="bg-white pl-2 pr-2">Blue Cart</span>
           </div>
           <div className="text-center drop-shadow-lg">
-            <BlackBin
-              wasteDisplay={wasteDisplay}
-              reduceWasteDisplay={reduceWasteDisplay}
-            />
+            <BlackBin />
             <span className="bg-white pl-2 pr-2 ">Black Cart</span>
           </div>
           <div className="text-center drop-shadow-lg">
-            <GreenBin
-              wasteDisplay={wasteDisplay}
-              reduceWasteDisplay={reduceWasteDisplay}
-            />
+            <GreenBin />
             <span className="bg-white pl-2 pr-2">Green Cart</span>
           </div>
         </div>
